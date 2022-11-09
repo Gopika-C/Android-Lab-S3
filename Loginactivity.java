35996b8f60f35c733c5712604d00d2fa023a108d
Gopika Chingan <95700408+Gopika-C@users.noreply.github.com>
2022-11-09 15:39:53 +0530

    Add files via upload

--- Loginactivity.java
+++ Loginactivity.java
@@ -2,37 +2,49 @@ package com.example.database;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    EditText e1,e2,t;
+public class Loginactivity extends AppCompatActivity {
+
+    EditText e1,e2;
     Button b1;
+    TextView t1;
 
     private DBMS_Class dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_loginactivity);
 
         e1=(EditText) findViewById(R.id.e1);
         e2=(EditText) findViewById(R.id.e2);
+        t1=(TextView) findViewById(R.id.t1);
 
         b1=(Button) findViewById(R.id.button);
 
-        dbHandler = new DBMS_Class(MainActivity.this);
-
+        dbHandler = new DBMS_Class(Loginactivity.this);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                dbHandler.save_login(e1.getText().toString(), e2.getText().toString());
-                Toast.makeText(MainActivity.this, "login Details Stored.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Trying to Login,....", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
             }
         });
     }
